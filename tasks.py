876aa13c5245c7d387ed9cd09e6aa07804fc6478
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-04-04 20:59:10 +0300

    lisattiin invoke test automaatio

--- tasks.py
+++ tasks.py
@@ -1,13 +1,18 @@
+import pty
 from invoke import task
 @task
 def start(ctx):
-    ctx.run("poetry python run src/index.py", pty=True)
+    ctx.run("poetry run python3 src/index.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("poetry run pytest src", pty=True)
 
 @task
 def coverage(ctx):
     ctx.run("poetry run coverage run --branch -m pytest src", pty=True)
 
-@task
+@task(coverage)
 def coverage_report(ctx):
     ctx.run("poetry coverage html", pty=True)
 
