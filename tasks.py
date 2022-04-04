dd6654ab18ae9de4e937f605864e7598480f8c0b
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-04-04 18:13:38 +0300

    luotiin invoke automaatiot

---
+++ tasks.py
@@ -0,0 +1,14 @@
+import imp
+from invoke import task
+@task
+def start(ctx):
+    ctx.run("python run src/index.py", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest", pty=True)
+
+@task
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
+
