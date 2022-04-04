ef8e4ff517ba603712b80f4dae80f1e55e8a4a3e
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-04-04 16:06:49 +0300

    abstraktoitiin ruudukko omaan luokkaansa

---
+++ src/gridDisplay.py
@@ -0,0 +1,68 @@
+from tkinter import *
+class gridDisplay:
+    def __init__(self, canvas, gridWidth, gridHeight, cellWidth, cellHeight, displayTextOffsetX, disPlayTextOffSetY, fontSize, maxLettersInCell):
+       
+        self.canvas = canvas
+        self.cellGridValues = {}
+       
+        self.gridWidth = gridWidth
+        self.gridHeight = gridHeight
+        self.cellWidth = cellWidth
+        self.cellHeight = cellHeight
+       
+        self.displayTextOffsetX = displayTextOffsetX
+        self.disPlayTextOffSetY = disPlayTextOffSetY
+        self.previewTextFontWidth  = fontSize 
+       
+        self.fontSize = fontSize
+        self.maxLettersInCell = maxLettersInCell
+       
+        for column in range(gridWidth):
+            for row in range(gridHeight):
+                placeholder = f"{column}|{row}"
+                newCell = self.canvas.create_rectangle(column*self.cellWidth, row*cellHeight, column*self.cellWidth+self.cellWidth, row*self.cellHeight+self.cellHeight, tags=("clickable"))        
+                newCellDisplayTextId = self.canvas.create_text(column*self.cellWidth+self.displayTextOffsetX, row*self.cellHeight+self.disPlayTextOffSetY, text=placeholder, anchor=NW, justify=LEFT, width=self.cellWidth-5, font=str(self.fontSize))
+                self.cellGridValues[newCellDisplayTextId] = placeholder
+
+       
+
+
+        #textCanvasWidget is the id of the Text widget that spawns when a rectanlge is clicked
+        self.TextCanvasWidget = None
+        
+        #textCanvasIten is the id of the texCanvasWidget window inside the canvas
+        self.TextCanvasItem = None
+        
+        #displayTextId is the raw text shown when a rectangle is not clicked
+        self.DisplayTextId = None
+   
+    def onClick(self, event):
+        virtualCoords = self.clipToGrid(self.canvas.canvasx(event.x), self.canvas.canvasy(event.y))
+        if self.DisplayTextId != None and self.TextCanvasWidget != None and self.cellGridValues != None:
+            newText = self.TextCanvasWidget.get("1.0", END)
+            self.canvas.itemconfig(self.DisplayTextId, text=self.generatePreviewText(newText))
+            self.cellGridValues[self.DisplayTextId] = newText
+            self.canvas.delete(self.TextCanvasItem)
+            self.TextCanvasItem = None
+        
+    
+
+        displayTextBoxId = self.canvas.find_closest(virtualCoords[0], virtualCoords[1])
+        self.DisplayTextId = displayTextBoxId[0]+1
+        textOfDisplayBox = self.cellGridValues[self.DisplayTextId]
+        self.TextCanvasWidget = Text(self.canvas, state=NORMAL, font=str(self.fontSize))
+        self.TextCanvasWidget.insert("1.0", textOfDisplayBox)
+    
+        self.TextCanvasWidget.focus_set()
+        self.TextCanvasItem = self.canvas.create_window(virtualCoords[0], virtualCoords[1],width=self.cellWidth, height=self.cellHeight, anchor=NW, window=self.TextCanvasWidget)    
+  
+    def generatePreviewText(self, text):
+       # maxLenghtInChars = self.cellWidth // self.fontsize 
+        return text.replace("\n", "")[:self.maxLettersInCell]
+
+    def clipToGrid(self, firstx, firsty):
+        x = firstx - firstx % self.cellWidth
+        y = firsty - firsty  % self.cellHeight
+        return (x, y)
+
+        
--- src/index.py
+++ src/index.py
@@ -1,12 +1,3 @@
-
-
-
-
-
-
-
-
-
 from cgitb import text
 from threading import currentThread
 from tkinter import *
@@ -16,6 +7,7 @@ import math
 from tkinter import ttk
 from webbrowser import get
 
+from gridDisplay import gridDisplay
 
 
 root = Tk()
@@ -29,89 +21,39 @@ root['menu'] = menubar
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
+
 menu_file = Menu(menubar)
 menubar.add_cascade(menu=menu_file, label='Tiedosto')
 
 menu_edit = Menu(menubar)
 menubar.add_cascade(menu=menu_edit, label='Muokkkaa')
-width = 10
-
-vbar=Scrollbar(root,orient=HORIZONTAL)
 
 
+hbar=Scrollbar(root, orient=HORIZONTAL)
+vbar=Scrollbar(root, orient=VERTICAL)
 
-gridCanvas = Canvas(root, bg="white", height=250, width=300, xscrollcommand = vbar.set)
-#viewPort = Frame(gridCanvas, height=1000, width=1000)
-#gridCanvas.create_window(0, 0, window=viewPort)
-
-
+gridWidth = 100
+gridHeight = 100
+cellWidth = 200
+cellHeight = 50
+cellDisplayTextOffsetpxX = 1
+cellDisplayTextOffsetpxY = 3
+fontsize = 12
+maxLettersInCell = 44
+gridCanvas = Canvas(root, bg="white", height=250, width=300, xscrollcommand = hbar.set, yscrollcommand=vbar.set)
 gridCanvas.grid(column=0, row=0, sticky=W+E+S+N)
+spreadSheetView = gridDisplay(gridCanvas, gridWidth, gridHeight, cellWidth, cellHeight, cellDisplayTextOffsetpxX, cellDisplayTextOffsetpxY, fontsize, maxLettersInCell)
 
 
-
-vbar.config(command=gridCanvas.xview)
-
-vbar.grid(column=0, row=1, sticky=EW)
-columnwidth = 100
-columnHeight = 16
-
-def clipToGrid(firstx, firsty):
-    x = firstx - firstx % columnwidth
-    y = firsty - firsty  % columnHeight
-    return (x, y)
-def generatePreviewText(text, maxLenght):
-    return text.replace("\n", "")[:maxLenght]
-    
+vbar.config(command=gridCanvas.yview)
+#vbar.grid(column=1, row=0, columnspan=2)
 
 
-TextEditorCanvasId = None
-TextEditorCanvasWidget = None
-DisplayTextToEdit = None
-def onclick(event):
-    global TextEditorCanvasId
-    global cellGridValues
-    global DisplayTextToEdit
-    global TextEditorCanvasWidget
-    virtualCoords = clipToGrid(gridCanvas.canvasx(event.x), gridCanvas.canvasy(event.y))
-    screenCoords = clipToGrid(event.x, event.y)
-    if TextEditorCanvasId != None and TextEditorCanvasWidget != None and cellGridValues != None and DisplayTextToEdit != None:
-        newText = TextEditorCanvasWidget.get("1.0", END)
-        gridCanvas.itemconfig(DisplayTextToEdit, text=generatePreviewText(newText, 10))
-        cellGridValues[DisplayTextToEdit] = newText
-        gridCanvas.delete(TextEditorCanvasId)
-        TextEditorCanvasId = None
-    
-   
+hbar.config(command=gridCanvas.xview)
+hbar.grid(column=0, row=1, sticky=E+W)
 
-    displayTextBoxId = gridCanvas.find_closest(screenCoords[0], screenCoords[1])
-    DisplayTextToEdit = displayTextBoxId[0]+1
-    textOfDisplayBox = cellGridValues[DisplayTextToEdit]
-    TextEditorCanvasWidget = Text(gridCanvas, state=NORMAL)
-    TextEditorCanvasWidget.insert("1.0", textOfDisplayBox)
-   
-    TextEditorCanvasWidget.focus_set()
-    TextEditorCanvasId = gridCanvas.create_window(virtualCoords[0], virtualCoords[1],width=columnwidth, height=columnHeight*2, anchor=NW, window=TextEditorCanvasWidget)
-    
-    
-
-
-
-cellGridValues = {}
-for column in range(100):
-    for row in range(100):
-      
-    #    EditText = Text(gridCanvas)
-        
-     #   gridCanvas.create_window(column*columnwidth, row*columnHeight, window=EditText)
-       placeholder = "kokeilu"
-       newCell = gridCanvas.create_rectangle(column*columnwidth, row*columnHeight, column*columnwidth+columnwidth, row*columnHeight+columnHeight, tags=("clickable"))        
-       newCellText = gridCanvas.create_text(column*columnwidth, row*columnHeight+2, text=placeholder, anchor=NW, justify=LEFT, width=columnwidth-5)
-       cellGridValues[newCellText] = placeholder
-       
-    
-
-gridCanvas.configure(scrollregion = [0,0,100*columnwidth, 100*columnHeight])
-gridCanvas.bind('<1>', onclick)
+gridCanvas.configure(scrollregion = [0, 0, gridWidth*cellWidth, gridHeight*cellHeight])
+gridCanvas.bind('<1>', lambda event: spreadSheetView.onClick(event))
 
 
 
