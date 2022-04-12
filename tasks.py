9900538479d5e79b5e76d8b84029eeeb2c949992
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-04-12 19:21:26 +0300

    lisätiin invoke komento pylintille

--- tasks.py
+++ tasks.py
@@ -19,3 +19,7 @@ def coverage(ctx):
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("poetry run coverage html", pty=True)
+
+@task
+def lint(ctx):
+    ctx.run("poetry run pylint src", pty=True)
