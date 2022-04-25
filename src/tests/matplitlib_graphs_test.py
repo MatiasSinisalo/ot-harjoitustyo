992bbdf906d86986c1214aeca552e31ad5ea018e
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-04-25 18:58:44 +0300

    lisättiiin unittest piirakkataulukolle

--- src/tests/matplitlib_graphs_test.py
+++ src/tests/matplitlib_graphs_test.py
@@ -15,24 +15,51 @@ class Test_matplitlib_graphs(unittest.TestCase):
         self.chartManager = ChartManager(self.canvas)
         self.canvas.grid(column = 0, row=0)
         
-        self.barChart = self.chartManager.add_new_bar_chart("test", "x_title", "y_title", [1,2,3], [10,20,30], 6, 4, 100, 100, 100)
-        self.barChartWindow =  self.chartManager.get_barchart_widget_canvas_item(self.barChart)
+       
+      
     def test_bar_chart_is_created(self):
-        #test if the chartmanager has created an chart canvas object
-        self.assertEqual(self.barChart.widgetName, "chartWidget")
+        #test if the chartmanager has created an bar chart canvas object
+        barChart = self.chartManager.add_new_bar_chart("test", "x_title", "y_title", [1,2,3], [10,20,30], 6, 4, 100, 100, 100)
+        barChartWindow =  self.chartManager.get_chart_widget_canvas_item(barChart)
+        self.assertEqual(barChart.widgetName, "chartWidget")
+    
+    def test_bar_chart_is_created(self):
+        #test if the chartmanager has created an bar pie canvas object
+        pieChart = self.chartManager.add_new_pie_chart("test", "x_title", "y_title", [1,2,3], [10,20,30], 6, 4, 100, 100, 100) 
+        self.assertEqual(pieChart.widgetName, "chartWidget")
     
     def test_bar_chart_can_be_moved(self):
         #First drag event sets the start coord of the drag, but doesnt move it
+        barChart = self.chartManager.add_new_bar_chart("test", "x_title", "y_title", [1,2,3], [10,20,30], 6, 4, 100, 100, 100)
+        barChartWindow =  self.chartManager.get_chart_widget_canvas_item(barChart)
         dragevent = Event(0, 0)
-        dragevent.widget = self.barChart
+        dragevent.widget = barChart
         dragevent.state = 256
         self.chartManager.on_chart_drag(dragevent)
-        self.assertEqual(self.canvas.coords(self.barChartWindow)[0], 100.0)
-        self.assertEqual(self.canvas.coords(self.barChartWindow)[1], 100.0)
+        self.assertEqual(self.canvas.coords(barChartWindow)[0], 100.0)
+        self.assertEqual(self.canvas.coords(barChartWindow)[1], 100.0)
 
         #second event moves the chart
         dragevent.x = 100
         dragevent.y = 100
         self.chartManager.on_chart_drag(dragevent)
-        self.assertEqual(self.canvas.coords(self.barChartWindow)[0], 200.0)
-        self.assertEqual(self.canvas.coords(self.barChartWindow)[1], 200.0)
+        self.assertEqual(self.canvas.coords(barChartWindow)[0], 200.0)
+        self.assertEqual(self.canvas.coords(barChartWindow)[1], 200.0)
+    
+    def test_pie_chart_can_be_moved(self):
+        #First drag event sets the start coord of the drag, but doesnt move it
+        pieChart = self.chartManager.add_new_pie_chart("test", "x_title", "y_title", [1,2,3], [10,20,30], 6, 4, 100, 100, 100)
+        pieChartWindow =  self.chartManager.get_chart_widget_canvas_item(pieChart)
+        dragevent = Event(0, 0)
+        dragevent.widget = pieChart
+        dragevent.state = 256
+        self.chartManager.on_chart_drag(dragevent)
+        self.assertEqual(self.canvas.coords(pieChartWindow)[0], 100.0)
+        self.assertEqual(self.canvas.coords(pieChartWindow)[1], 100.0)
+
+        #second event moves the chart
+        dragevent.x = 100
+        dragevent.y = 100
+        self.chartManager.on_chart_drag(dragevent)
+        self.assertEqual(self.canvas.coords(pieChartWindow)[0], 200.0)
+        self.assertEqual(self.canvas.coords(pieChartWindow)[1], 200.0)
