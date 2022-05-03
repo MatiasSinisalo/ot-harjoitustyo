9b6c8349eec56eea521dfecc3a9de8b6c71a9a5d
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-05-03 17:32:15 +0300

    added tests to spread_sheet_app

---
+++ src/tests/spread_sheet_app_test.py
@@ -0,0 +1,85 @@
+
+import unittest
+import json
+from spread_sheet_app import SpreadSheetApp
+from tests.custom_event import Event
+class TestSpreadSheetApp(unittest.TestCase):
+    def setUp(self):
+        self.app = SpreadSheetApp()
+        self.app.init_front_end()
+    
+    def test_app_inits_correctly(self):
+        self.assertIsNot(self.app.spread_sheet_view, None)
+        self.assertIsNot(self.app.canvas_chart_manager, None)
+    
+    def test_app_saves_cells_correctly(self):
+        self.app.saveStateToFile()
+        try:
+            file = open("test.json")
+            data = json.load(file)
+            cellValues = data["cell_values"]
+            self.assertEqual(cellValues["1"], 0)
+        except:
+            self.assertEqual("something failed when trying to read save file", False)
+    
+    def test_app_saves_edited_cells_correctly(self):
+        self.app.spread_sheet_view.cell_grid_values[1] = "This is a test"
+        self.app.saveStateToFile()
+        try:
+            file = open("test.json")
+            data = json.load(file)
+            cellValues = data["cell_values"]
+            self.assertEqual(cellValues["1"], "This is a test")
+            self.app.spread_sheet_view.cell_grid_values[1] = 0
+        except:
+            self.assertEqual("something failed when trying to read save file", False)
+    
+    def test_app_saves_bar_chart_correctly(self):
+        newChart = self.app.canvas_chart_manager.add_new_bar_chart("test", "x_title", "y_title", [1,2,3], [10,20,30], 6, 4, 100, 100, 100)
+        removeEvent = Event(100.0, 100.0)
+        removeEvent.widget = newChart
+        self.app.saveStateToFile()
+        
+        file = open("test.json")
+        data = json.load(file)
+        charts = data["chart_values"]
+        chart_values = charts["0"]
+        self.assertEqual(chart_values["type"], "Bar")
+        self.assertEqual(chart_values["title"], "test")
+        self.assertEqual(chart_values["x_title"], "x_title")
+        self.assertEqual(chart_values["y_title"], "y_title")
+        self.assertEqual(chart_values["x_values"], [1, 2, 3])
+        self.assertEqual(chart_values["y_values"], [10, 20, 30])
+        self.assertEqual(chart_values["dots"], 100)
+        self.assertEqual(chart_values["coords"], [100.0, 100.0])
+        self.app.canvas_chart_manager.delete_chart(removeEvent)
+        
+    
+    def test_app_saves_pie_chart_correctly(self):
+        newChart = self.app.canvas_chart_manager.add_new_pie_chart("test", "x_title", "y_title", [1,2,3], [10,20,30], 6, 4, 100, 100, 100)
+        removeEvent = Event(100.0, 100.0)
+        removeEvent.widget = newChart
+        self.app.saveStateToFile()
+        
+        file = open("test.json")
+        data = json.load(file)
+        charts = data["chart_values"]
+        chart_values = charts["0"]
+        self.assertEqual(chart_values["type"], "Pie")
+        self.assertEqual(chart_values["title"], "test")
+        self.assertEqual(chart_values["x_title"], "x_title")
+        self.assertEqual(chart_values["y_title"], "y_title")
+        self.assertEqual(chart_values["x_values"], [1, 2, 3])
+        self.assertEqual(chart_values["y_values"], [10, 20, 30])
+        self.assertEqual(chart_values["dots"], 100)
+        self.assertEqual(chart_values["coords"], [100.0, 100.0])
+        self.app.canvas_chart_manager.delete_chart(removeEvent)
+        
+           
+      
+
+
+       
+
+
+        
