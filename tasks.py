03cac31f899b26677b84b3a5201af0dc6231eb3c
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-05-03 18:49:17 +0300

    added back tasks.py

---
+++ tasks.py
@@ -0,0 +1,25 @@
+from invoke import task
+
+
+@task
+def start(ctx):
+    ctx.run("poetry run python3 src/index.py", pty=True)
+
+
+@task
+def test(ctx):
+    ctx.run("poetry run pytest src", pty=True)
+
+
+@task
+def coverage(ctx):
+    ctx.run("poetry run coverage run --branch -m pytest src", pty=True)
+
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("poetry run coverage html", pty=True)
+
+@task
+def lint(ctx):
+    ctx.run("poetry run pylint src", pty=True)
